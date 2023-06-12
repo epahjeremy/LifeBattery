9f08e28ac9e762d8c2474a4c4a6f9845a791bc30
epahjeremy <103704527+epahjeremy@users.noreply.github.com>
2023-06-12 17:00:46 +0200

    Update lifebattery.py

--- lifebattery.py
+++ lifebattery.py
@@ -1,1 +1,23 @@
 import pandas as pd
+import time
+from datetime import datetime, date
+
+
+
+    
+def open_who_data(): 
+  stats_path = './files/who_data.csv'
+  with open(stats_path, 'r') as file:
+        who_data = file.read()
+        df = pd.read_csv(who_data)
+        df = df[df["Unnamed: 1"] == "2019"] 
+        return df
+      
+
+who_df = open_who_data()      
+
+
+
+
+
+    
